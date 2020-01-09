3b848fa5f156a7ca08638d531eb4bf0dd204ec5e
Andrew Donaldson <andrew@sendowl.com>
2020-01-09 15:02:29 +0000

    Ugly commit: linting + add wolfram CDF

--- lib/mimemagic/overlay.rb
+++ lib/mimemagic/overlay.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 # Extra magic
 
-[['application/vnd.openxmlformats-officedocument.presentationml.presentation', [[0, "PK\003\004", [[30, '[Content_Types].xml', [[0..10000, 'ppt/']]]]]]],
- ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', [[0, "PK\003\004", [[30, '[Content_Types].xml', [[0..10000, 'xl/']]]]]]],
+[['application/vnd.openxmlformats-officedocument.presentationml.presentation', [[0, "PK\003\004", [[30, '[Content_Types].xml', [[0..10_000, 'ppt/']]]]]]],
+ ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', [[0, "PK\003\004", [[30, '[Content_Types].xml', [[0..10_000, 'xl/']]]]]]],
  ['application/vnd.ms-excel.sheet.macroEnabled.12', [[0, "Something That Doesn't Exist"]]],
- ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', [[0, "PK\003\004", [[30, '[Content_Types].xml', [[0..10000, 'word/']]]]]]],
- ['application/octet-stream', [[0, "<x:xmpmeta" ]]]].each do |magic|
+ ['application/vnd.openxmlformats-officedocument.wordprocessingml.document', [[0, "PK\003\004", [[30, '[Content_Types].xml', [[0..10_000, 'word/']]]]]]],
+ ['application/vnd.wolfram.cdf.text', [[0, '(* Content-type: application/vnd.wolfram.cdf.text *)']]],
+ ['application/octet-stream', [[0, '<x:xmpmeta']]]].each do |magic|
   MimeMagic.add(magic[0], magic: magic[1])
 end
--- spec/lib/mimemagic_spec.rb
+++ spec/lib/mimemagic_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rspec'
 require 'mimemagic'
 require 'stringio'
@@ -5,9 +7,9 @@ require 'forwardable'
 
 RSpec.describe 'MimeMagic' do
   it 'should have type, mediatype and subtype' do
-    expect( MimeMagic.new('text/html').type).to eq 'text/html'
-    expect( MimeMagic.new('text/html').mediatype).to eq 'text'
-    expect( MimeMagic.new('text/html').subtype).to eq 'html'
+    expect(MimeMagic.new('text/html').type).to eq 'text/html'
+    expect(MimeMagic.new('text/html').mediatype).to eq 'text'
+    expect(MimeMagic.new('text/html').subtype).to eq 'html'
   end
 
   it 'should have mediatype helpers' do
@@ -27,7 +29,7 @@ RSpec.describe 'MimeMagic' do
   end
 
   it 'should have extensions' do
-    expect(MimeMagic.new('text/html').extensions).to eq %w(htm html)
+    expect(MimeMagic.new('text/html').extensions).to eq %w[htm html]
   end
 
   it 'should have comment' do
@@ -52,48 +54,55 @@ RSpec.describe 'MimeMagic' do
   end
 
   it 'should recognize xlsx as zip without overlay' do
-    file = "spec/fixtures/files/application.vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    file = 'spec/fixtures/files/application.vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     expect(MimeMagic.by_magic(File.read(file))).to eq 'application/zip'
     expect(MimeMagic.by_magic(File.open(file, 'rb'))).to eq 'application/zip'
   end
 
   it 'should recognize xlsm with overlay, but only if we can see the file path' do
-    require "mimemagic/overlay"
+    require 'mimemagic/overlay'
 
-    file = "spec/fixtures/bad_magic_files/sample.xlsm"
+    file = 'spec/fixtures/bad_magic_files/sample.xlsm'
     expect(MimeMagic.by_magic(File.read(file))).to eq 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     expect(MimeMagic.by_magic(File.open(file, 'rb'))).to eq 'application/vnd.ms-excel.sheet.macroEnabled.12'
   end
 
   it 'should recognize pptx with overlay, but only if we can see the file path' do
-    require "mimemagic/overlay"
+    require 'mimemagic/overlay'
 
-    file = "spec/fixtures/bad_magic_files/sample.pptx"
+    file = 'spec/fixtures/bad_magic_files/sample.pptx'
     expect(MimeMagic.by_magic(File.read(file))).to eq 'application/zip'
     expect(MimeMagic.by_magic(File.open(file, 'rb'))).to eq 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
   end
 
   it 'should recognize a weirdly formatted pptx with overlay, but only if we can see the file path' do
-    require "mimemagic/overlay"
+    require 'mimemagic/overlay'
 
-    file = "spec/fixtures/bad_magic_files/sample.pptx"
+    file = 'spec/fixtures/bad_magic_files/sample.pptx'
     expect(MimeMagic.by_magic(File.read(file))).to eq 'application/zip'
     expect(MimeMagic.by_magic(File.open(file, 'rb'))).to eq 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
   end
 
   it 'should recognize by magic' do
-    require "mimemagic/overlay"
+    require 'mimemagic/overlay'
     Dir['spec/fixtures/files/*'].each do |file|
-      mime = File.split(file).last.sub('.', '/')
-      expect(MimeMagic.by_magic(File.read(file))).to eq(mime), "#{file} is not a #{mime}. This can happen if you add a file to spec/fixtures/files and the detection code relies on the file extension and your file does not have one"
-      expect(MimeMagic.by_magic(File.open(file, 'rb'))).to eq(mime), "#{file} is not a #{mime}. This can happen if you add a file to spec/fixtures/files and the detection code relies on the file extension and your file does not have one"
+      expected = File.split(file).last.sub('.', '/')
+      actual = MimeMagic.by_magic(File.read(file))
+      expect(actual).to eq(expected), "contents: #{file} is not #{expected}, it's #{actual}. This can happen if you add a file to spec/fixtures/files and the detection code relies on the file extension and your file does not have one"
+      actual_bin = MimeMagic.by_magic(File.open(file, 'rb'))
+      expect(actual_bin).to eq(expected), "file: #{file} is not #{expected}, it's #{actual_bin}. This can happen if you add a file to spec/fixtures/files and the detection code relies on the file extension and your file does not have one"
     end
   end
 
-  it 'should recognize xmp' do
-    require "mimemagic/overlay"
+  it 'should recognize wolfram cdf files' do
+    require 'mimemagic/overlay'
+    file = 'spec/fixtures/bad_magic_files/sample.cdf'
+    expect(MimeMagic.by_magic(File.read(file))).to eq 'application/vnd.wolfram.cdf.text'
+  end
 
-    file = "spec/fixtures/bad_magic_files/sample.xmp"
+  it 'should recognize xmp' do
+    require 'mimemagic/overlay'
+    file = 'spec/fixtures/bad_magic_files/sample.xmp'
     expect(MimeMagic.by_magic(File.read(file))).to eq 'application/octet-stream'
   end
 
@@ -106,22 +115,22 @@ RSpec.describe 'MimeMagic' do
 
   it 'should have add' do
     MimeMagic.add('application/mimemagic-test',
-                  extensions: %w(ext1 ext2),
+                  extensions: %w[ext1 ext2],
                   parents: 'application/xml',
                   comment: 'Comment')
     expect(MimeMagic.by_extension('ext1')).to eq 'application/mimemagic-test'
     expect(MimeMagic.by_extension('ext2')).to eq 'application/mimemagic-test'
     expect(MimeMagic.by_extension('ext2').comment).to eq 'Comment'
-    expect(MimeMagic.new('application/mimemagic-test').extensions).to eq %w(ext1 ext2)
+    expect(MimeMagic.new('application/mimemagic-test').extensions).to eq %w[ext1 ext2]
     expect(MimeMagic.new('application/mimemagic-test')).to be_child_of 'text/plain'
   end
 
   it 'should process magic' do
     MimeMagic.add('application/mimemagic-test',
                   magic: [[0, 'MAGICTEST'], # MAGICTEST at position 0
-                             [1, 'MAGICTEST'], # MAGICTEST at position 1
-                             [9..12, 'MAGICTEST'], # MAGICTEST starting at position 9 to 12
-                             [2, 'MAGICTEST', [[0, 'X'], [0, 'Y']]]]) # MAGICTEST at position 2 and (X at 0 or Y at 0)
+                          [1, 'MAGICTEST'], # MAGICTEST at position 1
+                          [9..12, 'MAGICTEST'], # MAGICTEST starting at position 9 to 12
+                          [2, 'MAGICTEST', [[0, 'X'], [0, 'Y']]]]) # MAGICTEST at position 2 and (X at 0 or Y at 0)
 
     expect(MimeMagic.by_magic('MAGICTEST')).to eq 'application/mimemagic-test'
     expect(MimeMagic.by_magic('XMAGICTEST')).to eq 'application/mimemagic-test'
@@ -134,16 +143,16 @@ RSpec.describe 'MimeMagic' do
     expect(MimeMagic.by_magic('Y MAGICTEST')).to eq 'application/mimemagic-test'
     expect(MimeMagic.by_magic('Z MAGICTEST')).to be_nil
 
-    expect(MimeMagic.by_magic(StringIO.new 'MAGICTEST')).to eq 'application/mimemagic-test'
-    expect(MimeMagic.by_magic(StringIO.new 'XMAGICTEST')).to eq 'application/mimemagic-test'
-    expect(MimeMagic.by_magic(StringIO.new ' MAGICTEST')).to eq 'application/mimemagic-test'
-    expect(MimeMagic.by_magic(StringIO.new '123456789MAGICTEST')).to eq 'application/mimemagic-test'
-    expect(MimeMagic.by_magic(StringIO.new '123456789ABMAGICTEST')).to eq 'application/mimemagic-test'
-    expect(MimeMagic.by_magic(StringIO.new '123456789ABCMAGICTEST')).to eq 'application/mimemagic-test'
-    expect(MimeMagic.by_magic(StringIO.new '123456789ABCDMAGICTEST')).to be_nil
-    expect(MimeMagic.by_magic(StringIO.new 'X MAGICTEST')).to eq 'application/mimemagic-test'
-    expect(MimeMagic.by_magic(StringIO.new 'Y MAGICTEST')).to eq 'application/mimemagic-test'
-    expect(MimeMagic.by_magic(StringIO.new 'Z MAGICTEST')).to be_nil
+    expect(MimeMagic.by_magic(StringIO.new('MAGICTEST'))).to eq 'application/mimemagic-test'
+    expect(MimeMagic.by_magic(StringIO.new('XMAGICTEST'))).to eq 'application/mimemagic-test'
+    expect(MimeMagic.by_magic(StringIO.new(' MAGICTEST'))).to eq 'application/mimemagic-test'
+    expect(MimeMagic.by_magic(StringIO.new('123456789MAGICTEST'))).to eq 'application/mimemagic-test'
+    expect(MimeMagic.by_magic(StringIO.new('123456789ABMAGICTEST'))).to eq 'application/mimemagic-test'
+    expect(MimeMagic.by_magic(StringIO.new('123456789ABCMAGICTEST'))).to eq 'application/mimemagic-test'
+    expect(MimeMagic.by_magic(StringIO.new('123456789ABCDMAGICTEST'))).to be_nil
+    expect(MimeMagic.by_magic(StringIO.new('X MAGICTEST'))).to eq 'application/mimemagic-test'
+    expect(MimeMagic.by_magic(StringIO.new('Y MAGICTEST'))).to eq 'application/mimemagic-test'
+    expect(MimeMagic.by_magic(StringIO.new('Z MAGICTEST'))).to be_nil
   end
 
   it 'should handle different file objects' do
@@ -154,7 +163,7 @@ RSpec.describe 'MimeMagic' do
       end
 
       extend Forwardable
-      delegate [:read, :size, :rewind, :eof?, :close] => :@io
+      delegate %i[read size rewind eof? close] => :@io
     end
     expect(MimeMagic.by_magic(IOObject.new)).to eq 'application/mimemagic-test'
     class StringableObject
